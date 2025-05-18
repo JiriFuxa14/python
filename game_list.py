a471726abb39db525c47c26e8c686a7c6127fe1a
Jiří Fuxa <jiri.fuxa024@skolavdf.cz>
2025-05-18 22:03:43 +0200

    Add files via upload

---
+++ game_list.py
@@ -0,0 +1,4 @@
+games=["Chess", "Snake", "Tetris", "Pacman"]
+games.append("8Ball")
+games.pop(2)
+print(games)
---
+++ gta_6.py
@@ -0,0 +1,7 @@
+def grand_theft_auto_vi():
+    game=['unfinished', 'finished']
+    if 'finished' or 'unfinished' in game:
+        print("Grand Theft Auto VI is finished.")
+    else:
+        print("Grand Theft Auto VI is still in development.")
+grand_theft_auto_vi()
