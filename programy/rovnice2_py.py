288e6b955dd69d2b108aa96117357208d7c3e5b0
Jiří Fuxa <jiri.fuxa024@skolavdf.cz>
2025-06-03 15:01:49 +0200

    Add files via upload

---
+++ programy/rovnice2_py.py
@@ -0,0 +1,10 @@
+x = 3
+y = 5
+rovnice_L = 4 * x + 3
+rovnice_R = 5 * y - 1
+rovnice_cela = rovnice_L < rovnice_R
+if rovnice_cela:
+    rovnice_cela = True
+    print("Podmínka je splněna")
+else:
+    print("Podmínka není splněna")
